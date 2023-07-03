c9015b622db9168cb4f4c8682b55f3a7161a0cba
Andy Lopez <ndy.loredo@gmail.com>
2023-07-03 11:08:24 -0400

    Reached POST route and added JSON data via Postman

--- routes/users.js
+++ routes/users.js
@@ -7,13 +7,25 @@ const users = [
     firstName: "John",
     lastName: "Doe",
     age: 25
+  },
+  {
+    firstName: "Jane",
+    lastName: "Doe",
+    age: 24
   }
 ]
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
   console.log(users);
+
   res.send(users);
+});
+
+router.post('/', (req, res) => {
+  console.log('POST ROUTE REACHED');
+
+  res.send('POST ROUTE REACHED');
 });
 
 export default router;
