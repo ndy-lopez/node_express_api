5c819fa1875e4a795432075760c515876e93b77e
Andy Lopez <ndy.loredo@gmail.com>
2023-07-03 11:00:31 -0400

    Displaying our first user JSON data

--- routes/users.js
+++ routes/users.js
@@ -2,9 +2,18 @@ import express from 'express';
 
 const router = express.Router();
 
+const users = [
+  {
+    firstName: "John",
+    lastName: "Doe",
+    age: 25
+  }
+]
+
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
-  res.send('Hello');
+  console.log(users);
+  res.send(users);
 });
 
 export default router;
