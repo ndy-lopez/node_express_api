764bb97b4668c96b7ac773c7fa0ea2d7541ef8d5
Andy Lopez <ndy.loredo@gmail.com>
2023-07-03 11:24:22 -0400

    added unique ID to users in DB and set up the ID route

--- routes/users.js
+++ routes/users.js
@@ -1,19 +1,9 @@
 import express from 'express';
+import { v4 as uuidv4 } from 'uuid';
 
 const router = express.Router();
 
-const users = [
-  {
-    firstName: "John",
-    lastName: "Doe",
-    age: 25
-  },
-  {
-    firstName: "Jane",
-    lastName: "Doe",
-    age: 24
-  }
-]
+const users = [];
 
 // all routes in here are starting with /users
 router.get('/', (req, res) => {
@@ -21,11 +11,16 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-  const user =req.body
+  const user = req.body;
 
-  users.push(user);
+  users.push({ ...user, id: uuidv4() });
 
   res.send(`User with the username ${user.firstName} added to the database!`);
+});
+
+
+router.get('/:id', (req, res) => {
+  res.send('The GET ID route');
 });
 
 export default router;
