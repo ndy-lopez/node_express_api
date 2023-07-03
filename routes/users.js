e6433da4a822724e5bf3be37b369222586ca030a
Andy Lopez <ndy.loredo@gmail.com>
2023-07-03 12:14:53 -0400

    Completed routes and refactored code - tested every CRUD actions are working

--- routes/users.js
+++ routes/users.js
@@ -1,54 +1,21 @@
 import express from 'express';
-import { v4 as uuidv4 } from 'uuid';
+
+import { createUsers } from '../controllers/users.js';
+import { getUser } from '../controllers/users.js';
+import { getUsers } from '../controllers/users.js';
+import { deleteUser } from '../controllers/users.js';
+import { updateUser } from '../controllers/users.js';
 
 const router = express.Router();
 
-let users = [];
+router.get('/', getUsers)
 
-// all routes in here are starting with /users
-router.get('/', (req, res) => {
-  res.send(users);
-});
+router.post('/', createUsers);
 
-router.post('/', (req, res) => {
-  const user = req.body;
+router.get('/:id', getUser);
 
-  users.push({ ...user, id: uuidv4() });
+router.delete('/:id', deleteUser);
 
-  res.send(`User with the username ${user.firstName} added to the database!`);
-});
-
-// /users/2 => req.params { id: 2 }
-
-router.get('/:id', (req, res) => {
-  const { id } = req.params;
-
-  const foundUser = users.find((user) => user.id === id);
-
-  res.send(foundUser);
-});
-
-router.delete('/:id', (req, res) => {
-  const { id } = req.params;
-
-  users = users.filter((user) => user.id !== id);
-
-  res.send(`User with the id ${id} deleted from the database`);
-});
-
-
-router.patch('/:id', (req, res) => {
-  const { id } = req.params;
-  const { firstName, lastName, age } = req.body;
-
-  const user = users.find((user) => user.id === id);
-
-  if(firstName) user.firstName = firstName;
-  if(lastName) user.lastName = lastName;
-  if(age) user.age = age;
-
-  res.send(`User with the id ${id} has been updated`);
-
-});
+router.patch('/:id', updateUser);
 
 export default router;
